0d61f1bcd181c9a3745f9a62be55503eb365f016
sheef-code <shifathsalam@gmail.com>
2019-06-10 13:53:18 -0700

    refactored code, completed project

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 
 class SearchBar extends React.Component {
-  state = { term: "" };
+  state = { term: '' };
+
   onFormSubmit = event => {
     event.preventDefault();
+
     this.props.onSubmit(this.state.term);
   };
+
   render() {
     return (
       <div className="ui segment">
